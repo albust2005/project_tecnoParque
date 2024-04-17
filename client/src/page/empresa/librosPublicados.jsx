bebeea521b2240a4e97c4e08bb7c3665f6a192de
Jkose7 <ojosema10@gmail.com>
2024-04-17 00:45:01 -0500

    maquetacion de los libros empresa

--- client/src/page/empresa/librosPublicados.jsx
+++ client/src/page/empresa/librosPublicados.jsx
@@ -5,18 +5,39 @@ export function LibrosPublicados() {
 
 
     return (
-        <section>
-            <h1>Tus libros</h1>
+        <section className="w-full flex flex-col items-center mt-20 gap-6">
+            <div>
+                <h1 className="font-bold text-5xl">Tus libros</h1>
+            </div>
             {
                 libros?.length === 0
                     ?
                     <h1>Aun no haz publicado ningun libro</h1>
                     :
-                    libros?.map(libro =>(
-                        <div key={libro.id}>
-                            <h1>{libro.titulo}</h1>
-                        </div>
-                    ))
+                    <section className="mx-auto grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 max-w-1000">
+                        {
+                            libros?.map(libro => (
+                                <div key={libro.id} className="w-full rounded-md bg-black text-white">
+                                    <div>
+                                        <img src="../src/assets/img/libros.jpg" className="rounded-t-md max-h-96 w-full object-cover" alt="" />
+                                    </div>
+                                    <div className="p-3 flex flex-col gap-2">
+                                        <div>
+                                            <h1 className="font-semibold text-wrap">{libro.titulo}</h1>
+                                            <p className="">{libro.descripcion}</p>
+                                            <p>costo: {libro.costo}</p>
+                                        </div>
+                                        <div className="flex flex-row-reverse gap-3 text-black">
+                                            <button className="bg-neutral-100 px-2 min-h-4 rounded-sm">Editar</button>
+                                            <button className="bg-neutral-100 px-2 min-h-4 rounded-sm">Eliminar</button>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            ))
+                        }
+                    </section>
+
             }
         </section>
     )
