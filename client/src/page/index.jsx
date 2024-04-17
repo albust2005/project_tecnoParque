1795bdc1f4a798e4d28e7970d82a971a95143588
NicoNico04 <nicollcanocastro465@gmail.com>
2024-04-17 02:13:33 -0500

    img_index

--- client/src/page/index.jsx
+++ client/src/page/index.jsx
@@ -12,7 +12,7 @@ export function Index() {
         <>
             <div className="flex w-full h-[80vh] overflow-hidden z-10 justify-center items-center text-white font-serif">
                 <div className="flex flex-col h-[80%] w-full absolute bg-gray-500 z-10 opacity-70 text-center items-center justify-center"></div>
-                <img src="../../src/assets/img/imagenLibro.webp" alt="" className="w-full object-cover -z-10"/>
+                <img src="../../src/assets/img/index.webp" alt="" className="w-full object-fill -z-10"/>
                 <h1 className="absolute z-20 text-8xl font-serif mb-[10%]">Empresa ABC</h1>
                 <p className="absolute z-20 mt-[8%] w-[80%] text-center text-3xl">
                     Los libros son faros en la oscuridad, guiándonos hacia la sabiduría y la comprensión.
